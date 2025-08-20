b23537d185f4db05f6cfb02d7c4660144b3fcd0f
ahmedehab72 <a01011618272@gmail.com>
2025-08-20 17:45:20 +0300

    update

--- src/components/Others/TalentSection.tsx
+++ src/components/Others/TalentSection.tsx
@@ -17,9 +17,9 @@ const TalentSection = () => {
 
     }}>
         <h2 className="text-white text-lg mt-[-80px] tracking-[.5rem]">TALENT</h2>
-    <div className="flex w-2/5 mx-auto relative items-center gap-2 ">
+    <div className="flex w-full lg:w-2/5 mx-auto relative items-center gap-2 ">
       <Input className="bg-white text-black h-18 " type="email" placeholder="ENTER JOB TITLE, KEYWORDS OR MISION." />
-      <button className="absolute right-0 h-18 py-4 px-12 bg-[#f1e4db] hover:bg-[#f3daca] border-none rounded-none shadow-none" type="submit">
+      <button className="absolute right-0 h-18 py-4 lg:px-12 px-4 bg-[#f1e4db] hover:bg-[#f3daca] border-none rounded-none shadow-none" type="submit">
         Ok
       </button>
     </div>    </div>
